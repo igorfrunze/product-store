0ea3767d493381bd930c673f1ca4ce46c16964c3
igor_frunze <igor_frunze@live.com>
2025-09-12 22:21:54 +0300

    implement backend, product CRUD

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+import productRoutes from './routes/product.route.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
+app.listen(3003, () => {
+  connectDB();
+  console.log('Server started at http://localhost:3003');
+});
